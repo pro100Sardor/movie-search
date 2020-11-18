36a4a93194809735f7dbb6ee13daec826df1b566
pro100Sardor <sardorbek.juraqulov@gmail.com>
2020-11-18 21:26:12 +0500

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,70 @@
+var elSearchForm = $_ ('#searchForm');
+var elUserSearch = $_ ('#userSearch', elSearchForm);
+
 var elPageMain = $_ ('#pageMain');
 
-function createCinemaItem (itemFeatureTitle, itemFeatureText) {
-  var item = document.createElement('li');
-  item.setAttribute('class', 'mb-4');
 
-  var cinemaItemFeatureTitle = document.createElement('span');
-  cinemaItemFeatureTitle.setAttribute('class', 'font-weight-bold');
-  cinemaItemFeatureTitle.textContent = itemFeatureTitle;
+var normalizedMovies = movies.map(function (movie, i) {
+  return {
+    id: i + 1,
+    title: movie.Title.toString(),
+    year: movie.movie_year,
+    categories: movie.Categories.split('|'),
+    summary: movie.summary,
+    imdbId: movie.imdb_id,
+    imdbRating: movie.imdb_rating,
+    runtime: movie.runtime,
+    language: movie.language,
+    youtubeId: movie.ytid
+  }
+});
+
+function createCinemaItem (itemFeatureTitle, itemFeatureText) {
+  var item = createElement('li', 'mb-4 px-3');
+
+  var cinemaItemFeatureTitle = createElement ('span', 'font-weight-bold', itemFeatureTitle);
 
   item.appendChild(cinemaItemFeatureTitle);
   item.innerHTML += itemFeatureText;
   return item;
 }
 
-movies.forEach(function(movie) {
-  var cinemaList = document.createElement('ul');
-  cinemaList.setAttribute('class', 'list-unstyled w-50 mb-4');
+elSearchForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
 
-  cinemaList.appendChild(createCinemaItem('Title: ', movie.title));
-  cinemaList.appendChild(createCinemaItem('${keys[1]}: ', movie.year));
-  cinemaList.appendChild(createCinemaItem('${keys[2]}: ', movie.cast));
-  cinemaList.appendChild(createCinemaItem('${keys[3]}: ', movie.genres));
+  var userSearch = elUserSearch.value.trim();
 
-  elPageMain.appendChild(cinemaList);
+  var searchRegex = new RegExp(userSearch, 'gi');
+
+  elPageMain.innerHTML = '';
+
+  normalizedMovies.forEach(function(movie){
+    if (movie.title.match(searchRegex)) {
+      var cinemaList = createElement('ul', 'movies-list list-unstyled mb-4 bg-white rounded pt-3 shadow');
+
+      cinemaList.appendChild(createCinemaItem('№: ', movie.id));
+      cinemaList.appendChild(createCinemaItem('Title: ', movie.title));
+      cinemaList.appendChild(createCinemaItem('Year: ', movie.year));
+      cinemaList.appendChild(createCinemaItem('Category: ', movie.categories));
+      cinemaList.appendChild(createCinemaItem('Summary: ', movie.summary));
+      cinemaList.appendChild(createCinemaItem('IMDB id: ', movie.imdbId));
+      cinemaList.appendChild(createCinemaItem('IMDB Reiting: ', movie.imdbRating));
+      cinemaList.appendChild(createCinemaItem('Runtime: ', movie.runtime));
+      cinemaList.appendChild(createCinemaItem('Language: ', movie.language));
+
+      var cinemaTriller = document.createElement('a');
+      cinemaTriller.href = `http://youtube.com/watch?v=${movie.youtubeId}`;
+      cinemaTriller.target = '_blank';
+      cinemaTriller.textContent = movie.youtubeId;
+      cinemaList.appendChild(createCinemaItem('Triller: ', cinemaTriller));
+
+      elPageMain.appendChild(cinemaList);
+    }
+  });
+
+  if (elPageMain.innerHTML === '') {
+    elPageMain.appendChild(createElement('p', 'text-white', 'Bunday titleli kino topilmadi'));
+  }
+
 });
 
